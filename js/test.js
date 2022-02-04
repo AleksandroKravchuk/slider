4b13707e7f3c05574caeeaa3794d727061f1ac6b
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-02-04 21:03:33 +0200

    1

---
+++ js/test.js
@@ -0,0 +1,12 @@
+const name = "Aleksandr";
+let number = "43px";
+const bull = true;
+const bullN = false;
+
+
+// let should = prompt(name);
+// should = Number(should);
+// console.log(should);
+// console.log(typeof should)
+const result = Number.parseInt(number);
+console.log(result)
