0af6dde7462fae1e7802e02dadbdc38fab01e935
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-02-15 21:07:06 +0200

    2

--- js/test.js
+++ js/test.js
@@ -569,3 +569,63 @@ for (let index = 0; index < link.length; index++) {
 }
 const numb = 12.4;
 console.log(Math.floor(numb))
+
+
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4.7,
+//     price: 5000,
+//     tags: ["premium", "promoted", "top", "trusted"],
+//     owner: {
+//         name: "Henry Sibola",
+//         phone: "982-126-1588",
+//         email: "henry.carter@aptmail.com",
+//     },
+// };
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//     country: "Jamaica",
+//         city: "Kingston",
+// }
+// console.log(apartment)
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = [];
+const values = [];
+for (const key in apartment) {
+    keys.push(key);
+    values.push(apartment[key]); 
+} 
+console.log(keys);
+console.log(values);
+
+
+// ПИРИМЕР КОРЗИНЫ ПОКУПКИ ПРОДУКТОВ
+
+const cart = {
+    items: [],
+    getItems() { },
+    add(product) { },
+    remove(productName) { },
+    clear() { },
+    countTotalPrice() { },
+    increaseQuantity(productName) { },
+    decreaseQuantity(productName) {},
+}
+
+
+cart.add({ name: , price: 50 });
+cart.add({ name: , price: 60 });
+cart.add({ name: , price: 60 });
+cart.add({ name: , price: 110 });
+
+
+
+cart.remove();
