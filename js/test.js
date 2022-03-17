101267182fbb8411a064477ec8186262f716f758
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-03-17 19:15:33 +0200

    1

--- js/test.js
+++ js/test.js
@@ -1135,11 +1135,10 @@ const pizzaPalace = {
 
 
 
-const getUsersWithFriend = (users, friendName) => {
-  
-    // return users.filter((user) => user.friends === friendName);
-    console.log(users.filter((user) => user.friends === friendName))
-   console.log(friendName)
+const getUsersWithFriend = (users) => {
+    const friend = users.flatMap(user => user.friends);
+ return friend.filter((name, index, array) => array.indexOf(name) ===index)
+       
 };
 
     console.log(getUsersWithFriend([
@@ -1213,15 +1212,15 @@ const getUsersWithFriend = (users, friendName) => {
     gender: "female",
     age: 39
   }
-], "Sharron Pace"));
+],));
 
 
 
 const getUsersWithAge = (users, minAge, maxAge) => {
- return users.filter((user)=>user.age === maxAge)
+ return users.filter((user)=>user.friends.includes("Sharron Pace") )
 
 };
-console.log(getUsersWithAge([
+console.table(getUsersWithAge([
   {
     name: "Moore Hensley",
     email: "moorehensley@indexia.com",
@@ -1293,3 +1292,79 @@ console.log(getUsersWithAge([
     age: 39
   }
 ],10,37))
+
+
+
+const calculateT= users => {
+  return  users.reduce((acc, user) => acc + user.balance,0)
+         
+  
+   
+};
+
+
+console.log(calculateT([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]))
