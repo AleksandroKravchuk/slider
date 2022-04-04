16bb3b0b0f17c692a46a588fe1f57a4ecd8e88f3
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-04 21:37:49 +0300

    2

--- js/test.js
+++ js/test.js
@@ -1901,5 +1901,31 @@ const makeChange = function () {
 // swpColor(`biue`)
 console.log(document)
 
-const non = document.querySelector(`.wrapper`)
-console.log(non)
+const calkr = {
+  value: 0,
+  increment() {
+    this.value -= 1;
+  },
+  dicrement() {
+    this.value += 1;
+  }
+}
+const incrementBtn = document.querySelector(`.js-increment`);
+const dicrementBtn = document.querySelector(`.js-dicremeht`);
+const valueBtn = document.querySelector(`.js-value`);
+
+// incrementBtn.textContent = `Вика привет`
+
+incrementBtn.addEventListener(`click`, function () {
+  calkr.increment();
+  valueBtn.textContent = calkr.value;
+})
+
+dicrementBtn.addEventListener(`click`, function () {
+  calkr.dicrement();
+  valueBtn.textContent = calkr.value;
+})
+
+console.log(incrementBtn)
+console.log(dicrementBtn)
+console.log(valueBtn)
